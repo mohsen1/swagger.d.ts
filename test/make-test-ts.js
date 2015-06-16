47dfae3ba1ed2100c516b6a8fdcb4d2113762a1c
Mohsen Azimi <msn.azi@gmail.com>
2015-06-16 15:17:28 -0700

    More test

---
+++ test/make-test-ts.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+var examplesFolder = '../node_modules/swagger-validator/examples/v2.0/json/';
+examplesFolder = path.join(__dirname, examplesFolder);
+
+var ts = '/// <reference path="../swagger.d.ts"/>\n\n';
+
+fs.readdirSync(examplesFolder)
+  .forEach(function (exampleFile, i) {
+    var exampleFilePath = path.join(examplesFolder, exampleFile);
+
+    if (fs.statSync(exampleFilePath).isFile()) {
+      ts += 'var spec' + i + ' : Swagger.Spec = ' +
+        fs.readFileSync(exampleFilePath).toString() + ';\n\n';
+    }
+  });
+
+fs.writeFileSync('test.ts', ts);
